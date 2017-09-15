a2c849c825c016bc66296814e9f03003a62e4c12
Chris Dawson <chrisdawson1996@gmail.com>
2017-09-15 14:29:19 +0100

    Initial Commit

---
+++ SkyLine2D/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkyLine2D
+//
+//  Created by Chris Dawson on 15/09/2017.
+//  Copyright © 2017 Chris Dawson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
