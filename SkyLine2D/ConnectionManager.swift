5e0f889d4af12a41005b765d558c968d397064ba
chris <chrisdawson1996@gmail.com>
2018-06-19 14:34:02 +0100

    Finished and commented this prototype version of the app

--- SkyLine2D/ConnectionManager.swift
+++ SkyLine2D/ConnectionManager.swift
@@ -12,7 +12,12 @@ class ConnectionManager {
     var connected: Bool
     var port: String
     var socketFileDescriptor: Int32
+    // The message to be sent to the skyline pc
     var command: UnsafeMutablePointer<Int8> = UnsafeMutablePointer.allocate(capacity: 1)
+    // A buffer which stores and communications from the skyline pc
+    // A buffer of 85 bytes (since a CChar is 1 byte long) should be enough to store any communications
+    // coming from the skyline pc. This size will need to be updated if the messages become longer
+    var receiveBuf: UnsafeMutablePointer<CChar> = UnsafeMutablePointer.allocate(capacity: 85)
     
     init() {
         connected = false
@@ -24,6 +29,9 @@ class ConnectionManager {
     
     // This had to be done in a bit of an unusual way for Swift since all the functions
     // for socket programming are C functions
+    // A very useful guide to demystify some of these functions can be found here :
+    // https://beej.us/guide/bgnet/html/single/bgnet.html
+    // If this link doesn't work search for "Beej's guide to network programming"
     func connectTo(address: String, port: String) {
         var hints = addrinfo();
         var serverInfo: UnsafeMutablePointer<addrinfo>?;
@@ -34,9 +42,11 @@ class ConnectionManager {
         let status = getaddrinfo(address, port, &hints, &serverInfo)
         
         if(status >= 0) {
+            // Open the socket
             socketFileDescriptor = socket((serverInfo?.pointee.ai_family)!, (serverInfo?.pointee.ai_socktype)!, (serverInfo?.pointee.ai_protocol)!)
             
             if(socketFileDescriptor >= 0) {
+                // Connect to the skyline pc
                 let connectionSuccess = connect(socketFileDescriptor, serverInfo?.pointee.ai_addr, (serverInfo?.pointee.ai_addrlen)!)
                 
                 if(connectionSuccess < 0) {
@@ -56,18 +66,57 @@ class ConnectionManager {
         
     }
     
+    // Sends a message to the skyline pc
+    // In the current version the message sent is a number which indicates which
+    // buttons has been pressed
     func sendMessage(message: Int8) {
         if(connected) {
             var bytesSent: Int
             command.pointee = message
             
+            // Send the data
             bytesSent = send(socketFileDescriptor, command, MemoryLayout<Int8>.size, 0)
             
             if(bytesSent < 0) {
                 print("error: unable to send command")
+            } else {
+                // Sent successfully
+                print(bytesSent)
             }
         } else {
             print("error: Not connected")
+        }
+    }
+    
+    // Receives a message from the skyline pc and returns a String represenatation of that message
+    // Will return a String containing nothing if there was an error with reading
+    func receiveMessage() -> String {
+        if(connected) {
+            // Reset the contents of the receive buffer
+            receiveBuf.initialize(repeating: 0,count: 85)
+            
+            // Receive the message
+            let bytesReceived = recv(socketFileDescriptor, receiveBuf, (MemoryLayout<CChar>.size) * 85, 0)
+            
+            if(bytesReceived < 0) {
+                print("error reading")
+            } else {
+                // Message read successfully
+                print(bytesReceived)
+                return String(cString: receiveBuf)
+            }
+            return ""
+        } else {
+            print("error: Not connected")
+            return ""
+        }
+    }
+    
+    // Close the connection between the phone and the skyline pc
+    func closeConnection() {
+        if(connected) {
+            close(socketFileDescriptor)
+            print("Connection closed")
         }
     }
 }
--- SkyLine2D/ViewController.swift
+++ SkyLine2D/ViewController.swift
@@ -10,69 +10,70 @@ import UIKit
 import CoreFoundation
 
 class ViewController: UIViewController {
+    // Controls the connection to the skyline pc
     let connectionManager = ConnectionManager()
-    let topMenuItems: [String] = ["Run", "Measurements", "Settings", "Manual"]
-    let settingsMenuItems: [String] = ["Speeds", "Acceleration", "Deceleration", "Timeouts"]
-    var inTopMenu = true
-    var inSettingsMenu = false
-    var positionInList = 0
     
+    // The text boxes which display the text for each menu
     @IBOutlet weak var text1: UITextField!
     @IBOutlet weak var text2: UITextField!
     @IBOutlet weak var text3: UITextField!
     @IBOutlet weak var text4: UITextField!
     
+    // An array of the text boxes
     var textBoxes = [UITextField]()
     
+    // The cursor
     @IBOutlet weak var arrow: UIImageView!
     
+    // Connect to the skyline pc
+    // The address is the hostname of the pc, this is currently printed by the python program on startup
+    @IBAction func connect(_ sender: UIButton) {
+        connectionManager.connectTo(address: "biolpc3235", port: "9000")
+        setMenuText()
+    }
+    
+    @IBAction func closeConnection(_ sender: UIButton) {
+        connectionManager.closeConnection()
+    }
+    
+    // The following button presses send a message to the skyline pc indicating which button was pressed
+    // The messages sent are as follows:
+    // Up    -   1
+    // Down  -   2
+    // Left  -   3
+    // Right -   4
+    // Enter -   5
     @IBAction func enterButton(_ sender: UIButton) {
         print("Enter")
-        
-        if(positionInList == 2 && inTopMenu) {
-            for index in 0...textBoxes.count-1 {
-                textBoxes[index].text = settingsMenuItems[index]
-            }
-            setArrowPosition(position: 0)
-            inTopMenu = false
-            inSettingsMenu = true
-        }
+        connectionManager.sendMessage(message: 5)
+        setMenuText()
     }
     
     @IBAction func upButton(_ sender: UIButton) {
         print("Up")
-        arrow.center.y -= 40
-        positionInList -= 1
-        if(positionInList < 0) {
-            setArrowPosition(position: 3)
-        }
+        connectionManager.sendMessage(message: 1)
+        setMenuText()
     }
     
     @IBAction func downButton(_ sender: UIButton) {
         print("Down")
-        arrow.center.y += 40
-        positionInList += 1
-        if(positionInList > 3) {
-            setArrowPosition(position: 0)
-        }
+        connectionManager.sendMessage(message: 2)
+        setMenuText()
     }
     
     @IBAction func leftButton(_ sender: UIButton) {
         print("Left")
-        if(inSettingsMenu) {
-            for index in 0...textBoxes.count-1 {
-                textBoxes[index].text = topMenuItems[index]
-            }
-            setArrowPosition(position: 0)
-            inSettingsMenu = false
-            inTopMenu = true
-        }
+        connectionManager.sendMessage(message: 3)
+        setMenuText()
     }
     
     @IBAction func rightButton(_ sender: UIButton) {
         print("Right")
+        connectionManager.sendMessage(message: 4)
+        setMenuText()
     }
     
+    // The startup of the app
     override func viewDidLoad() {
         super.viewDidLoad()
         textBoxes = [text1, text2, text3, text4]
@@ -80,29 +81,55 @@ class ViewController: UIViewController {
         for text in textBoxes {
             text.isUserInteractionEnabled = false;
         }
-        
-        for index in 0...textBoxes.count-1 {
-            textBoxes[index].text = topMenuItems[index]
-        }
-
-        //connectionManager.connectTo(address: "192.168.137.1", port: "7000")
     }
     
+    // This function is generated automatically when you create an iOS project
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    // Sets the position of the cursor
     func setArrowPosition(position: Int) {
-        // Current y pos for option 1 is 78, and we move 40 between points
-        if(position >= 0 && position <= 3) {
-            switch position {
-            case 0:
-                arrow.center.y = 78 + 11
-            default :
-                arrow.center.y = (78 + 11) + CGFloat(position) * 40
-            }
-            positionInList = position
+        // The y position for the arrow to be lined up with the first text box is 89
+        // Any subsequent text boxes are positioned 40 lower than the previous one
+        if(position >= 1 && position <= 4) {
+            arrow.isHidden = false
+            arrow.center.y = 89 + (CGFloat(position-1) * 40)
+        } else {
+            // Hide the arrow if the cursor is outside of the range
+            // This will usually happen when the cursor is set to 0 on the skyline pc,
+            // this happens when the cursor should not be displayed
+            arrow.isHidden = true
+        }
+    }
+    
+    func setMenuText() {
+        // Receives the message corresponding to the menu text which should be displayed in the
+        // text boxes
+        let menuText = connectionManager.receiveMessage()
+        print(menuText)
+        
+        // Check to ensure the message contains data
+        if(!menuText.isEmpty) {
+            // The incoming message is in the form xx|xx|xx|xx where 'xx' represents some text that will be displayed
+            // I split this message into 4 pieces using the '|' character as the split point
+            let menus = menuText.split(separator: "|")
+            // Set the text box text
+            text1.text = String(menus[0])
+            text2.text = String(menus[1])
+            text3.text = String(menus[2])
+            text4.text = String(menus[3])
+        }
+        
+        // The message that immediately follows the menu text is the cursor position
+        // The cursor position will be between 1 and 4
+        let cursorPosition = connectionManager.receiveMessage()
+        print(cursorPosition)
+        
+        if(!cursorPosition.isEmpty) {
+            let position = Int(cursorPosition)!
+            // Set the phone cursor position to be the same as the skyline pc's cursor position
+            setArrowPosition(position: position)
         }
     }
 }
